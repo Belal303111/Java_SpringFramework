832867eb0f32b77d5f9158dcc0cb42988bc178e4
Belal30311 <011022bn@gmail.com>
2025-05-10 11:44:36 +0300

    Update

--- first_java_springboot_app/src/test/java/com/bebo/controller.java
+++ first_java_springboot_app/src/test/java/com/bebo/controller.java
@@ -2,12 +2,15 @@ package com.bebo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class controller{
     @GetMapping("/staffname")
-    public String getstaffdata()
+    public String getstaffdata(Staff model)
     {
+        model staff=new model("Belal","Software Engineer",1000000.898);
+        model.addAttribute("StaffName",staff);
         return "staffname";
     }
 }
